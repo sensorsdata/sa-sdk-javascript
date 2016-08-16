29a4168a5143af3ffe08aa7fae708a9c16354cf8
shengyonggen <spiritpanda@sina.com>
2016-08-16 12:13:24 +0800

    更新1.5

--- product/sensorsdata.js
+++ product/sensorsdata.js
@@ -3,17 +3,23 @@
  * @author shengyonggen@sensorsdata.cn
  */
 
+(function(sd) {
+
+// 防止重复引入
+sd = window[sd];
+
+if(typeof sd !== 'function' || sd.has_load_sdk){
+  return false;
+}
+sd.has_load_sdk = true;
+
 // 压缩后的json库
 if(typeof JSON!=='object'){JSON={}}(function(){'use strict';var rx_one=/^[\],:{}\s]*$/,rx_two=/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g,rx_three=/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,rx_four=/(?:^|:|,)(?:\s*\[)+/g,rx_escapable=/[\\\"\u0000-\u001f\u007f-\u009f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,rx_dangerous=/[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;function f(n){return n<10?'0'+n:n}function this_value(){return this.valueOf()}if(typeof Date.prototype.toJSON!=='function'){Date.prototype.toJSON=function(){return isFinite(this.valueOf())?this.getUTCFullYear()+'-'+f(this.getUTCMonth()+1)+'-'+f(this.getUTCDate())+'T'+f(this.getUTCHours())+':'+f(this.getUTCMinutes())+':'+f(this.getUTCSeconds())+'Z':null};Boolean.prototype.toJSON=this_value;Number.prototype.toJSON=this_value;String.prototype.toJSON=this_value}var gap,indent,meta,rep;function quote(string){rx_escapable.lastIndex=0;return rx_escapable.test(string)?'"'+string.replace(rx_escapable,function(a){var c=meta[a];return typeof c==='string'?c:'\\u'+('0000'+a.charCodeAt(0).toString(16)).slice(-4)})+'"':'"'+string+'"'}function str(key,holder){var i,k,v,length,mind=gap,partial,value=holder[key];if(value&&typeof value==='object'&&typeof value.toJSON==='function'){value=value.toJSON(key)}if(typeof rep==='function'){value=rep.call(holder,key,value)}switch(typeof value){case'string':return quote(value);case'number':return isFinite(value)?String(value):'null';case'boolean':case'null':return String(value);case'object':if(!value){return'null'}gap+=indent;partial=[];if(Object.prototype.toString.apply(value)==='[object Array]'){length=value.length;for(i=0;i<length;i+=1){partial[i]=str(i,value)||'null'}v=partial.length===0?'[]':gap?'[\n'+gap+partial.join(',\n'+gap)+'\n'+mind+']':'['+partial.join(',')+']';gap=mind;return v}if(rep&&typeof rep==='object'){length=rep.length;for(i=0;i<length;i+=1){if(typeof rep[i]==='string'){k=rep[i];v=str(k,value);if(v){partial.push(quote(k)+(gap?': ':':')+v)}}}}else{for(k in value){if(Object.prototype.hasOwnProperty.call(value,k)){v=str(k,value);if(v){partial.push(quote(k)+(gap?': ':':')+v)}}}}v=partial.length===0?'{}':gap?'{\n'+gap+partial.join(',\n'+gap)+'\n'+mind+'}':'{'+partial.join(',')+'}';gap=mind;return v}}if(typeof JSON.stringify!=='function'){meta={'\b':'\\b','\t':'\\t','\n':'\\n','\f':'\\f','\r':'\\r','"':'\\"','\\':'\\\\'};JSON.stringify=function(value,replacer,space){var i;gap='';indent='';if(typeof space==='number'){for(i=0;i<space;i+=1){indent+=' '}}else if(typeof space==='string'){indent=space}rep=replacer;if(replacer&&typeof replacer!=='function'&&(typeof replacer!=='object'||typeof replacer.length!=='number')){throw new Error('JSON.stringify')}return str('',{'':value})}}if(typeof JSON.parse!=='function'){JSON.parse=function(text,reviver){var j;function walk(holder,key){var k,v,value=holder[key];if(value&&typeof value==='object'){for(k in value){if(Object.prototype.hasOwnProperty.call(value,k)){v=walk(value,k);if(v!==undefined){value[k]=v}else{delete value[k]}}}}return reviver.call(holder,key,value)}text=String(text);rx_dangerous.lastIndex=0;if(rx_dangerous.test(text)){text=text.replace(rx_dangerous,function(a){return'\\u'+('0000'+a.charCodeAt(0).toString(16)).slice(-4)})}if(rx_one.test(text.replace(rx_two,'@').replace(rx_three,']').replace(rx_four,''))){j=eval('('+text+')');return typeof reviver==='function'?walk({'':j},''):j}throw new SyntaxError('JSON.parse')}}}());
 
 
 
 
-(function(sd) {
-
-  sd = window[sd];
-
-  var _ = sd._ = {};
+var _ = sd._ = {};
 
   
   // 默认配置
@@ -604,7 +610,7 @@ if(typeof JSON!=='object'){JSON={}}(function(){'use strict';var rx_one=/^[\],:{}
     , slice = ArrayProto.slice
     , toString = ObjProto.toString
     , hasOwnProperty = ObjProto.hasOwnProperty
-    , LIB_VERSION = '1.5.4';
+    , LIB_VERSION = '1.5.5';
 
 // 提供错误日志
   var error_msg = [];
@@ -661,7 +667,7 @@ if(typeof JSON!=='object'){JSON={}}(function(){'use strict';var rx_one=/^[\],:{}
     };
 
     _.logger = logger;
-
+    // 普通的extend，不能到二级
     _.extend = function(obj) {
       each(slice.call(arguments, 1), function(source) {
         for (var prop in source) {
@@ -672,7 +678,21 @@ if(typeof JSON!=='object'){JSON={}}(function(){'use strict';var rx_one=/^[\],:{}
       });
       return obj;
     };
-
+    // 允许二级的extend
+    _.extend2Lev = function(obj) {
+      each(slice.call(arguments, 1), function(source) {
+        for (var prop in source) {
+          if (source[prop] !== void 0) {
+            if(_.isObject(source[prop]) && _.isObject(obj[prop])){
+              _.extend(obj[prop],source[prop]);
+            }else{
+              obj[prop] = source[prop];
+            }
+          }
+        }
+      });
+      return obj;
+    };
     // 如果已经有的属性不覆盖,如果没有的属性加进来
     _.coverExtend = function(obj) {
       each(slice.call(arguments, 1), function(source) {
@@ -1010,9 +1030,9 @@ if(typeof JSON!=='object'){JSON={}}(function(){'use strict';var rx_one=/^[\],:{}
 
     return function() {
       // 有些浏览器取个屏幕宽度都异常...
-      var s = String(screen.height * screen.width);
-      if(s && /\d{5,}/.test(s)){
-        se = s.toString(16);        
+      var se = String(screen.height * screen.width);
+      if(se && /\d{5,}/.test(se)){
+        se = se.toString(16);        
       }else{
         se = String(Math.random() * 31242).replace('.','').slice(0,8);
       }
@@ -1309,9 +1329,8 @@ if(typeof JSON!=='object'){JSON={}}(function(){'use strict';var rx_one=/^[\],:{}
         $os_version: String(detector.os.version),
         $screen_height: Number(screen.height) || 0,
         $screen_width: Number(screen.width) || 0,
+        // 我说两遍写的重复，佳捷说就写两遍
         $lib: 'js',
-        $lib_method: 'code',
-//        $lib_detail: '',
         $lib_version: String(LIB_VERSION),
         $browser: detector.browser.name,
         $browser_version: String(detector.browser.version)
@@ -1534,6 +1553,11 @@ if(typeof JSON!=='object'){JSON={}}(function(){'use strict';var rx_one=/^[\],:{}
   saEvent.send = function(p, callback) {
     var data = {
       distinct_id: store.getDistinctId(),
+      lib:{
+        $lib: 'js',
+        $lib_method: 'code',
+        $lib_version: String(LIB_VERSION)
+      },
       properties: {}
     };
 
@@ -1581,10 +1605,15 @@ if(typeof JSON!=='object'){JSON={}}(function(){'use strict';var rx_one=/^[\],:{}
       data.jssdk_error = error_msg.join('--');
     }
 
-
+    // 合并properties里的属性
     if (_.isObject(p.properties) && !_.isEmptyObject(p.properties)) {
       _.extend(data.properties, p.properties);
     }
+    // 合并lib里的属性
+    if(_.isObject(callback)){
+      _.extend(data.lib,callback);
+    }
+
     // profile时不传公用属性
     if (!p.type || p.type.slice(0, 7) !== 'profile') {
       // 传入的属性 > 当前页面的属性 > session的属性 > cookie的属性 > 预定义属性
@@ -1980,6 +2009,7 @@ if(typeof JSON!=='object'){JSON={}}(function(){'use strict';var rx_one=/^[\],:{}
    * @param {string} groupid
    * */
   sd.trackAbtest = function(t, g) {
+    /*
     if (saEvent.check({test_id: t, group_id: g})) {
       saEvent.send({
         type: 'track_abtest',
@@ -1988,7 +2018,7 @@ if(typeof JSON!=='object'){JSON={}}(function(){'use strict';var rx_one=/^[\],:{}
           group_id: g
         }
       });
-    }
+    }*/
   };
 
   sd.registerPage = function(obj) {
