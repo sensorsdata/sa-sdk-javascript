0ac689c614f6f0da6294adaecf73c00b4465fc0d
shengyonggen <spiritpanda@sina.com>
2016-09-23 17:34:47 +0800

    增加latest相关，以及autoTrack的自定义

--- src/sdk.js
+++ src/sdk.js
@@ -528,7 +528,8 @@ _.urlParse = function(para) {
   };
   return new URLParser(para);
 }
-// 取host
+
+/* ulr预置
 _.referringDomain = function(referrer) {
   var split = referrer.split('/');
   if (split.length >= 3) {
@@ -536,7 +537,7 @@ _.referringDomain = function(referrer) {
   }
   return '';
 };
-// 取domain
+
 _.getDomainByHost = function(url) {
   if (typeof url === 'string' && url.split('.').length >= 2) {
     var temp = url.match(/[^\.]+\.[^.]+$/);
@@ -549,7 +550,7 @@ _.getDomainByHost = function(url) {
     return '';
   }
 }
-
+*/
 
 // 是否有标准的浏览器环境,如果不是发送$errorEnviroment:{$errorReson:'没有window'}
 _.hasStandardBrowserEnviroment = function() {
@@ -753,14 +754,203 @@ _.ajax = function(para) {
 
 };
 
+
+//https://github.com/websanova/js-url
+
+_.url = (function() {
+
+    function _t() {
+        return new RegExp(/(.*?)\.?([^\.]*?)\.?(com|net|org|biz|ws|in|me|co\.uk|co|org\.uk|ltd\.uk|plc\.uk|me\.uk|edu|mil|br\.com|cn\.com|eu\.com|hu\.com|no\.com|qc\.com|sa\.com|se\.com|se\.net|us\.com|uy\.com|ac|co\.ac|gv\.ac|or\.ac|ac\.ac|af|am|as|at|ac\.at|co\.at|gv\.at|or\.at|asn\.au|com\.au|edu\.au|org\.au|net\.au|id\.au|be|ac\.be|adm\.br|adv\.br|am\.br|arq\.br|art\.br|bio\.br|cng\.br|cnt\.br|com\.br|ecn\.br|eng\.br|esp\.br|etc\.br|eti\.br|fm\.br|fot\.br|fst\.br|g12\.br|gov\.br|ind\.br|inf\.br|jor\.br|lel\.br|med\.br|mil\.br|net\.br|nom\.br|ntr\.br|odo\.br|org\.br|ppg\.br|pro\.br|psc\.br|psi\.br|rec\.br|slg\.br|tmp\.br|tur\.br|tv\.br|vet\.br|zlg\.br|br|ab\.ca|bc\.ca|mb\.ca|nb\.ca|nf\.ca|ns\.ca|nt\.ca|on\.ca|pe\.ca|qc\.ca|sk\.ca|yk\.ca|ca|cc|ac\.cn|com\.cn|edu\.cn|gov\.cn|org\.cn|bj\.cn|sh\.cn|tj\.cn|cq\.cn|he\.cn|nm\.cn|ln\.cn|jl\.cn|hl\.cn|js\.cn|zj\.cn|ah\.cn|gd\.cn|gx\.cn|hi\.cn|sc\.cn|gz\.cn|yn\.cn|xz\.cn|sn\.cn|gs\.cn|qh\.cn|nx\.cn|xj\.cn|tw\.cn|hk\.cn|mo\.cn|cn|cx|cz|de|dk|fo|com\.ec|tm\.fr|com\.fr|asso\.fr|presse\.fr|fr|gf|gs|co\.il|net\.il|ac\.il|k12\.il|gov\.il|muni\.il|ac\.in|co\.in|org\.in|ernet\.in|gov\.in|net\.in|res\.in|is|it|ac\.jp|co\.jp|go\.jp|or\.jp|ne\.jp|ac\.kr|co\.kr|go\.kr|ne\.kr|nm\.kr|or\.kr|li|lt|lu|asso\.mc|tm\.mc|com\.mm|org\.mm|net\.mm|edu\.mm|gov\.mm|ms|nl|no|nu|pl|ro|org\.ro|store\.ro|tm\.ro|firm\.ro|www\.ro|arts\.ro|rec\.ro|info\.ro|nom\.ro|nt\.ro|se|si|com\.sg|org\.sg|net\.sg|gov\.sg|sk|st|tf|ac\.th|co\.th|go\.th|mi\.th|net\.th|or\.th|tm|to|com\.tr|edu\.tr|gov\.tr|k12\.tr|net\.tr|org\.tr|com\.tw|org\.tw|net\.tw|ac\.uk|uk\.com|uk\.net|gb\.com|gb\.net|vg|sh|kz|ch|info|ua|gov|name|pro|ie|hk|com\.hk|org\.hk|net\.hk|edu\.hk|us|tk|cd|by|ad|lv|eu\.lv|bz|es|jp|cl|ag|mobi|eu|co\.nz|org\.nz|net\.nz|maori\.nz|iwi\.nz|io|la|md|sc|sg|vc|tw|travel|my|se|tv|pt|com\.pt|edu\.pt|asia|fi|com\.ve|net\.ve|fi|org\.ve|web\.ve|info\.ve|co\.ve|tel|im|gr|ru|net\.ru|org\.ru|hr|com\.hr|ly|xyz)$/);
+    }
+
+    function _d(s) {
+      return decodeURIComponent(s.replace(/\+/g, ' '));
+    }
+
+    function _i(arg, str) {
+        var sptr = arg.charAt(0),
+            split = str.split(sptr);
+
+        if (sptr === arg) { return split; }
+
+        arg = parseInt(arg.substring(1), 10);
+
+        return split[arg < 0 ? split.length + arg : arg - 1];
+    }
+
+    function _f(arg, str) {
+        var sptr = arg.charAt(0),
+            split = str.split('&'),
+            field = [],
+            params = {},
+            tmp = [],
+            arg2 = arg.substring(1);
+
+        for (var i = 0, ii = split.length; i < ii; i++) {
+            field = split[i].match(/(.*?)=(.*)/);
+
+            // TODO: regex should be able to handle this.
+            if ( ! field) {
+                field = [split[i], split[i], ''];
+            }
+
+            if (field[1].replace(/\s/g, '') !== '') {
+                field[2] = _d(field[2] || '');
+
+                // If we have a match just return it right away.
+                if (arg2 === field[1]) { return field[2]; }
+
+                // Check for array pattern.
+                tmp = field[1].match(/(.*)\[([0-9]+)\]/);
+
+                if (tmp) {
+                    params[tmp[1]] = params[tmp[1]] || [];
+                
+                    params[tmp[1]][tmp[2]] = field[2];
+                }
+                else {
+                    params[field[1]] = field[2];
+                }
+            }
+        }
+
+        if (sptr === arg) { return params; }
+
+        return params[arg2];
+    }
+
+    return function(arg, url) {
+        var _l = {}, tmp, tmp2;
+
+        if (arg === 'tld?') { return _t(); }
+
+        url = url || window.location.toString();
+
+        if ( ! arg) { return url; }
+
+        arg = arg.toString();
+
+        if (tmp = url.match(/^mailto:([^\/].+)/)) {
+            _l.protocol = 'mailto';
+            _l.email = tmp[1];
+        }
+        else {
+
+            // Ignore Hashbangs.
+            if (tmp = url.match(/(.*?)\/#\!(.*)/)) {
+                url = tmp[1] + tmp[2];
+            }
+
+            // Hash.
+            if (tmp = url.match(/(.*?)#(.*)/)) {
+                _l.hash = tmp[2];
+                url = tmp[1];
+            }
+
+            // Return hash parts.
+            if (_l.hash && arg.match(/^#/)) { return _f(arg, _l.hash); }
+
+            // Query
+            if (tmp = url.match(/(.*?)\?(.*)/)) {
+                _l.query = tmp[2];
+                url = tmp[1];
+            }
+
+            // Return query parts.
+            if (_l.query && arg.match(/^\?/)) { return _f(arg, _l.query); }
+
+            // Protocol.
+            if (tmp = url.match(/(.*?)\:?\/\/(.*)/)) {
+                _l.protocol = tmp[1].toLowerCase();
+                url = tmp[2];
+            }
+
+            // Path.
+            if (tmp = url.match(/(.*?)(\/.*)/)) {
+                _l.path = tmp[2];
+                url = tmp[1];
+            }
+
+            // Clean up path.
+            _l.path = (_l.path || '').replace(/^([^\/])/, '/$1').replace(/\/$/, '');
+
+            // Return path parts.
+            if (arg.match(/^[\-0-9]+$/)) { arg = arg.replace(/^([^\/])/, '/$1'); }
+            if (arg.match(/^\//)) { return _i(arg, _l.path.substring(1)); }
+
+            // File.
+            tmp = _i('/-1', _l.path.substring(1));
+            
+            if (tmp && (tmp = tmp.match(/(.*?)\.(.*)/))) {
+                _l.file = tmp[0];
+                _l.filename = tmp[1];
+                _l.fileext = tmp[2];
+            }
+
+            // Port.
+            if (tmp = url.match(/(.*)\:([0-9]+)$/)) {
+                _l.port = tmp[2];
+                url = tmp[1];
+            }
+
+            // Auth.
+            if (tmp = url.match(/(.*?)@(.*)/)) {
+                _l.auth = tmp[1];
+                url = tmp[2];
+            }
+
+            // User and pass.
+            if (_l.auth) {
+                tmp = _l.auth.match(/(.*)\:(.*)/);
+
+                _l.user = tmp ? tmp[1] : _l.auth;
+                _l.pass = tmp ? tmp[2] : undefined;
+            }
+
+            // Hostname.
+            _l.hostname = url.toLowerCase();
+
+            // Return hostname parts.
+            if (arg.charAt(0) === '.') { return _i(arg, _l.hostname); }
+
+            // Domain, tld and sub domain.
+            if (_t()) {
+                tmp = _l.hostname.match(_t());
+
+                if (tmp) {
+                    _l.tld = tmp[3];
+                    _l.domain = tmp[2] ? tmp[2] + '.' + tmp[3] : undefined;
+                    _l.sub = tmp[1] || undefined;
+                }
+            }
+
+            // Set port and protocol defaults if not set.
+            _l.port = _l.port || (_l.protocol === 'https' ? '443' : '80');
+            _l.protocol = _l.protocol || (_l.port === '443' ? 'https' : 'http');
+        }
+
+        // Return arg.
+        if (arg in _l) { return _l[arg]; }
+
+        // Return everything.
+        if (arg === '{}') { return _l; }
+
+        // Default to undefined for no match.
+        return undefined;
+    };
+})();
+
+
+
+
 _.info = {
   initPage: function() {
     var referrer = document.referrer;
-    var referrer_host = referrer ? _.referringDomain(referrer) : referrer;
-    var referrer_domain = _.getDomainByHost(referrer_host);
+    var referrer_host = referrer ? _.url('hostname',referrer) : referrer;
+    var referrer_domain = referrer ? _.url('domain',referrer) : referrer;
     var url = location.href;
-    var url_host = url ? _.referringDomain(url) : url;
-    var url_domain = _.getDomainByHost(url_host);
+    var url_host = url ? _.url('hostname',url) : url;
+    var url_domain = url ? _.url('domain',url) : url;
     this.pageProp = {
       referrer: referrer,
       referrer_host: referrer_host,
@@ -790,12 +980,12 @@ _.info = {
 
     return params;
   },
-  campaignParamsStandard: function() {
+  campaignParamsStandard: function(prefix) {
     var utms = _.info.campaignParams();
     var $utms = {}, otherUtms = {};
     for (var i in utms) {
       if ((' ' + source_channel_standard + ' ').indexOf(' ' + i + ' ') !== -1) {
-        $utms['$' + i] = utms[i];
+        $utms[prefix + i] = utms[i];
       } else {
         otherUtms[i] = utms[i];
       }
@@ -921,7 +1111,7 @@ var saNewUser = {
       });
     }
     // utm
-    var $utms = _.info.campaignParamsStandard().$utms;
+    var $utms = _.info.campaignParamsStandard('$latest_').$utms;
     if (!_.isEmptyObject($utms)) {
       sd.register($utms);
     }
@@ -1223,7 +1413,7 @@ saEvent.send = function(p, callback) {
       }
       //判断新用户
       saNewUser.storeInitCheck();
-      //saNewUser.checkIsFirstLatest();
+      saNewUser.checkIsFirstLatest();
       // 如果初始化cookie失败，发送错误事件
       /*
        if(error_msg.length > 0 && sd.para.send_error_event){
@@ -1247,7 +1437,7 @@ saEvent.send = function(p, callback) {
       var _referrer = (document.referrer).slice(0, sd.para.max_referrer_string_length);
       sd.setOnceProfile({
         _init_referrer: _referrer,
-        _init_referrer_domain: _.referringDomain(_referrer)
+        _init_referrer_host: _.info.pageProp.referrer_host
       });
     },
     // set init sessionRegister cookie
@@ -1255,7 +1445,7 @@ saEvent.send = function(p, callback) {
       var _referrer = (document.referrer).slice(0, sd.para.max_referrer_string_length);
       store.setSessionPropsOnce({
         _session_referrer: _referrer,
-        _session_referrer_domain: _.referringDomain(_referrer)
+        _session_referrer_host: _.info.pageProp.referrer_host
       });
     },
     // set default referrr and pageurl
@@ -1263,11 +1453,16 @@ saEvent.send = function(p, callback) {
       _.info.register({
         _current_url: location.href,
         _referrer: (document.referrer).slice(0, sd.para.max_referrer_string_length),
-        _referring_domain: _.referringDomain(document.referrer)
+        _referring_host: _.info.pageProp.referrer_host
       });
     },
 
-    autoTrack: function() {
+    autoTrackWithoutProfile:function(para){
+      this.autoTrack(_.extend(para,{not_set_profile:true}));
+    },
+    autoTrack: function(para) {
+      para = _.isObject(para) ? para : {};
+
       var utms = _.info.campaignParams();
       var $utms = {};
       for (var i in utms) {
@@ -1277,24 +1472,28 @@ saEvent.send = function(p, callback) {
           $utms[i] = utms[i];
         }
       }
-      // setOnceProfile
-      if (is_first_visitor) {
+      // setOnceProfile 如果是新用户，且允许设置profile
+      if (is_first_visitor && !para.not_set_profile) {
         sd.setOnceProfile(_.extend({
             $first_visit_time: new Date(),
             $first_referrer: (document.referrer).slice(0, sd.para.max_referrer_string_length),
             $first_browser_language: navigator.language,
-            $first_referrer_host: _.referringDomain(document.referrer)
+            $first_referrer_host: _.info.pageProp.referrer_host
           }, $utms)
         );
       }
-      // trackpageview
+      if(para.not_set_profile){
+        delete para.not_set_profile;
+      }
+
+// trackpageview
       sd.track('$pageview', _.extend({
           $referrer: (document.referrer).slice(0, sd.para.max_referrer_string_length),
-          $referrer_host: _.referringDomain(document.referrer),
+          $referrer_host: _.info.pageProp.referrer_host,
           $url: location.href,
           $url_path: location.pathname,
           $title: document.title
-        }, $utms)
+        }, $utms,para)
       );
     }
 
@@ -1307,7 +1506,7 @@ saEvent.send = function(p, callback) {
     var arg0 = arg[0];
     var arg1 = arg.slice(1);
     if (typeof arg0 === 'string' && commonWays[arg0]) {
-      return commonWays[arg0].apply(sd, arg1);
+      return commonWays[arg0].apply(commonWays, arg1);
     } else if (typeof arg0 === 'function') {
       arg0.apply(sd, arg1);
     } else {
@@ -1567,16 +1766,13 @@ saEvent.send = function(p, callback) {
   };
 
 
-
-  sd.init = function() {
-    
+  sd.init = function() {    
     // 防止爬虫等异常情况
     /*
      if(!_.hasStandardBrowserEnviroment()){
      return false;
      }*/
     app_js_bridge();
-
     // 初始化referrer等页面属性 1.6
     _.info.initPage();
 
