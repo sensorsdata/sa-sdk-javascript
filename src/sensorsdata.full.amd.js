aef99b85cf6605ec0a0303dbda0c5e9977b6a471
shengyonggen <spiritpanda@sina.com>
2018-03-30 11:58:59 +0800

    修改版本

--- src/sensorsdata.full.amd.js
+++ src/sensorsdata.full.amd.js
@@ -143,7 +143,7 @@ var ObjProto = Object.prototype;
 var slice = ArrayProto.slice;
 var toString = ObjProto.toString;
 var hasOwnProperty = ObjProto.hasOwnProperty;
-var LIB_VERSION = '1.11.1';
+var LIB_VERSION = '1.10.1';
 
 sd.lib_version = LIB_VERSION;
 
