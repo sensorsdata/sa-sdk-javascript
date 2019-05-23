11cb24e87ed69f0d7720c636f99cfd79d05da30a
riyue <sheng.yonggeng@gmail.com>
2019-05-23 16:36:10 +0800

    更新

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
-var sensorsdata_js_sdk_latest_version = '1.13.7';
+var sensorsdata_js_sdk_latest_version = '1.13.8';
 if(typeof module ==='object' && typeof module.exports === 'object'){
 module.exports = sensorsdata_js_sdk_latest_version;
 }
