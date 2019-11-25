331783aa6d9ba36279456c2365f792b452655506
riyue <sheng.yonggeng@gmail.com>
2019-11-25 10:44:12 +0800

    更新

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
-var sensorsdata_js_sdk_latest_version = '1.14.13';
+var sensorsdata_js_sdk_latest_version = '1.14.14';
 if(typeof module ==='object' && typeof module.exports === 'object'){
 module.exports = sensorsdata_js_sdk_latest_version;
 }
