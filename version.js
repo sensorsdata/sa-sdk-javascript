92a2785edef243157c5b849c027a90f6065696aa
shengyonggen <shengyonggen@sensorsdata.cn>
2021-02-27 17:03:45 +0800

    Release 1.16.5

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
-var sensorsdata_js_sdk_latest_version ='1.16.4';
+var sensorsdata_js_sdk_latest_version ='1.16.5';
 if(typeof module ==='object' && typeof module.exports === 'object'){
 module.exports = sensorsdata_js_sdk_latest_version;
 }
