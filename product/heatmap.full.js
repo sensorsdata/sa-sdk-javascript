328d4ab8c4de78c59a91bdc639a5bbcee70d04a0
shengyonggen <shengyonggen@sensorsdata.cn>
2024-02-23 16:40:08 +0800

    Release 1.26.6

--- product/heatmap.full.js
+++ product/heatmap.full.js
@@ -9731,7 +9731,7 @@
 
     window.sa_jssdk_heatmap_render = function(se, data, type, url) {
       sd = se;
-      sd.heatmap_version = '1.26.5';
+      sd.heatmap_version = '1.26.6';
       _ = sd._;
       _.querySelectorAll = function(val) {
         if (typeof val !== 'string') {
